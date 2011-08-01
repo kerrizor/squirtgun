bef275352c3c8bb1ca834c995353a3264cb5362f
Kerri Miller <kerri@vholdr.com>
2011-07-31 17:16:56 -0700

    it really needs to accept a hash instead of a string

--- lib/squirtgun.rb
+++ lib/squirtgun.rb
@@ -1,11 +1,15 @@
 class Squirtgun
   require 'socket'
 
-  def report(message)
+  def report(options={})
+    return if options[:event].nil? or options[:value].nil?
+
+    # TODO: send this as JSON
+    message = "#{options[:event]}:#{options[:value]}"
+
     # TODO: add message format testing?  Guzzler currenlty expects [context]:[value]
     sock = UDPSocket.new
-
-    # TODO: need an environment variable for this value
+    # TODO: need an environment variable for these values
     sock.send(message, 0, '174.143.25.227', 43278)
     sock.close
   end
