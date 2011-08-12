5b697f9dbd3f1ad3c95a1ef82d92dbc6bb8694a3
Chris Doyle <archslide@gmail.com>
2011-08-12 14:26:06 -0700

    class for config

--- lib/squirtgun.rb
+++ lib/squirtgun.rb
@@ -1,10 +1,21 @@
-module Squirtgun
-  require 'yaml'
+require 'cattr'
+require 'rubygems'
+require 'json'
 
-  # TODO: Read these values in from /config/squirtgun.yml
-  LISTENER      = '174.143.25.227'
-  LISTENER_PORT = '43278'
-  SECRET_KEY    = 'johnsmom'
+class Squirtgun
+
+  class Config
+    cattr_accessor :listener, :listener_port, :secret_key, :project_id
+
+    def self.listener
+      @@listener || '174.143.25.227'  # TODO: maybe there's a better way to do defaults?
+    end
+
+    def self.listener_port
+      @@listener_port || '43278'
+    end
+  end
+
 
   # TODO: decide if this is a shitty name because its all Squirtgun::Squirtgun..
   class Gun
@@ -17,8 +28,10 @@ module Squirtgun
 
       # TODO: add message format testing?  Guzzler currenlty expects [context]:[value]..
 
+      options[:project_id] ||= Squirtgun::Config.project_id
+
       sock = UDPSocket.new
-      sock.send({:stat => options, :hmac => encode_packet(options[:context] + options[:value], Squirtgun::SECRET_KEY)}.to_json, 0, Squirtgun::LISTENER, Squirtgun::LISTENER_PORT)
+      sock.send({:stat => options, :hmac => encode_packet(options[:context] + options[:value], Squirtgun::Config.secret_key)}.to_json, 0, Squirtgun::Config.listener, Squirtgun::Config.listener_port)
       sock.close
     end
 
