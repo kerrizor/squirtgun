9ab1df7cbdda8130f8f32216c15fe92acc8c612d
Kerri Miller <kerri@vholdr.com>
2011-08-11 19:31:35 -0700

    adding basic HMAC auth with hardcoded security values

--- lib/squirtgun.rb
+++ lib/squirtgun.rb
@@ -1,13 +1,30 @@
-class Squirtgun
-  require 'socket'
+module Squirtgun
+  require 'yaml'
 
-  def report(options={})
-    return if options[:context].nil? or options[:value].nil?
+  # TODO: Read these values in from /config/squirtgun.yml
+  LISTENER      = '174.143.25.227'
+  LISTENER_PORT = '43278'
+  SECRET_KEY    = 'johnsmom'
 
-    # TODO: add message format testing?  Guzzler currenlty expects [context]:[value]
-    sock = UDPSocket.new
-    # TODO: need an environment variable for these values
-    sock.send({:stat => options}.to_json, 0, '174.143.25.227', 43278)
-    sock.close
+  # TODO: decide if this is a shitty name because its all Squirtgun::Squirtgun..
+  class Gun
+    require 'socket'
+    require 'openssl'
+
+    def report(options={})
+      return if options[:context].nil? or options[:value].nil?
+
+
+      # TODO: add message format testing?  Guzzler currenlty expects [context]:[value]..
+
+      sock = UDPSocket.new
+      sock.send({:stat => options, :hmac => encode_packet(options[:context] + options[:value], Squirt::SECRET_KEY)}.to_json, 0, Squirt::LISTENER, Squirt::LISTENER_PORT)
+      sock.close
+    end
+
+    private
+      def encode_packet(data, key)
+        OpenSSL::HMAC.hexdigest('sha1', key, data)
+      end
   end
 end
